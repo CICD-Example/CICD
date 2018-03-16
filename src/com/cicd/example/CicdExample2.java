3c865fb312a2f93454e4293d186a5e835f9e9eed
smedise <smedise@LH7U05CG62603K9.ms.ds.uhc.com>
2018-03-16 11:00:33 -0700

    Testing

---
+++ src/com/cicd/example/CicdExample2.java
@@ -0,0 +1,11 @@
+package com.cicd.example;
+
+public class CicdExample2 {
+
+	public static void main(String[] args) {
+		System.out.println("Added another line to check if a build is triggered");
+		System.out.println("Lets try one more time");
+		
+	}
+
+}
