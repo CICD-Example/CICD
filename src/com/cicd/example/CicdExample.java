256f9d42483cf31f552b305dba7ecfc40f430a23
smedise <smedise@LH7U05CG62603K9.ms.ds.uhc.com>
2018-04-02 14:27:25 -0700

    test

--- src/com/cicd/example/CicdExample.java
+++ src/com/cicd/example/CicdExample.java
@@ -4,7 +4,7 @@ public class CicdExample {
 
 	public static void main(String[] args) {
 		System.out.println("Example printed successfully");
-		System.out.println("Added a new line to check if a build is triggered!!! Test!!");
+		System.out.println("Added a new line to check if a build is triggered!!! Huh!!");
 	}
 
 }
